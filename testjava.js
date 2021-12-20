090e8fb4fa3883123859d2bf67919531cf2a3b6a
Nguyenduc9x <nguyenvanduc.jvb@gmail.com>
2021-12-20 17:19:06 +0700

    add 2 file moi chay javascript

---
+++ testjava.js
@@ -0,0 +1,100 @@
+			var a = 2;
+            var b = 3;
+            var c = "Test";
+            var linebreak = "<br />";
+         
+            document.write("a + b = ");
+            result = a + b;
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("a - b = ");
+            result = a - b;
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("a / b = ");
+            result = a / b;
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("a % b = ");
+            result = a % b;
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("a + b + c = ");
+            result = a + b + c;
+            document.write(result);
+            document.write(linebreak);
+         
+            a = a++;
+            document.write("a++ = ");
+            result = a++;
+            document.write(result);
+            document.write(linebreak);
+         
+            b = b--;
+            document.write("b-- = ");
+            result = b--;
+            document.write(result);
+            document.write(linebreak);
+			
+			// so sanh
+			
+			document.write("(a & b) => ");
+            result = (a & b);
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("(a | b) => ");
+            result = (a | b);
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("(a ^ b) => ");
+            result = (a ^ b);
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("(~b) => ");
+            result = (~b);
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("(a << b) => ");
+            result = (a << b);
+            document.write(result);
+            document.write(linebreak);
+         
+            document.write("(a >> b) => ");
+            result = (a >> b);
+            document.write(result);
+            document.write(linebreak);
+			
+			var h = 10;
+            var e = "String";
+            var linebreak = "<br />";
+         
+            result = (typeof e == "string" ? "B is String" : "B is Numeric");
+            document.write("Result => ");
+            document.write(result);
+            document.write(linebreak);
+         
+            result = (typeof h == "string" ? "A is String" : "A is Numeric");
+            document.write("Result => ");
+            document.write(result);
+            document.write(linebreak);
+function callname(first, last)
+         {
+            var full;
+            full = first + last;
+            return full;
+         }
+         
+         function tinhnang2()
+         {
+            var result;
+            result = callname('aho', 'tana');
+            document.write (result );
+         }
